5b480ab2ad74923e2d8311294120ffdb69104a5b
Ahmed Toba <ahmdtwbt22@gmail.com>
2025-08-07 10:52:16 +0300

    enhance security by adding rate limiting & sending jwt via cookie & setting security HTTP headers & Data sanitization & Preventing Parameter pollution

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongosanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorControllers");
@@ -9,16 +14,54 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) MIDDLEWARES
+
+// Set security HTTP headers
+app.use("/api", helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour",
+});
+
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongosanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent paramater pollution
+app.use(
+    hpp({
+        whitelist: [
+            "duration",
+            "ratingsQuantity",
+            "ratingsAverage",
+            "maxGroupSize",
+            "difficulty",
+            "price",
+        ],
+    }),
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public/`));
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
-    console.log(req.headers);
+    // console.log(req.headers);
     next();
 });
 
